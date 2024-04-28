8f7c5d3cf0bf1473606c888ad6af6e5238e53d07
RayanaSales <rayanaasales@gmail.com>
2024-04-28 16:09:20 -0300

    Exercise 05: continuation

--- src/__tests__/exercise/05.js
+++ src/__tests__/exercise/05.js
@@ -5,8 +5,9 @@ import * as React from 'react'
 import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {build, fake} from '@jackfranklin/test-data-bot'
-import {rest} from 'msw'
 import {setupServer} from 'msw/node'
+import {rest} from 'msw'
+import {handlers} from '../../test/server-handlers'
 import Login from '../../components/login-submission'
 
 const buildLoginForm = build({
@@ -16,18 +17,15 @@ const buildLoginForm = build({
   },
 })
 
-const server = setupServer(
-  rest.post(
-    'https://auth-provider.example.com/api/login',
-    async (req, res, ctx) => {
-      return res(ctx.json({username: req.body.username}))
-    },
-  ),
-)
+const server = setupServer(...handlers)
+
 // the function setupServer is a function that returns a server object, the first argument represents the request that we want to mock, in this case we are mocking a post request to the url 'https://auth-provider.example.com/api/login', the second argument is a function that receives the request, the response and the context, and returns the response that we want to mock, in this case we are returning a json object with the username that we are receiving in the request body
 
 beforeAll(() => server.listen())
 afterAll(() => server.close())
+afterEach(
+  () => server.resetHandlers(), // Clean up all runtime handlers, remothe them all and reset to the original value back. This way the tests will not interfer with each other
+)
 
 test(`logging in displays the user's username`, async () => {
   render(<Login />)
@@ -40,4 +38,38 @@ test(`logging in displays the user's username`, async () => {
   await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
 
   expect(screen.getByText(username)).toBeInTheDocument()
+})
+
+test('omitting the password results in an error', async () => {
+  render(<Login />)
+  const {username} = buildLoginForm()
+
+  userEvent.type(screen.getByLabelText(/username/i), username)
+  // not going to fill the password field
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"password required"`,
+  )
+})
+
+test('unkown server error displays the error message', async () => {
+  const testErrorMessage = 'Oh no, something bad happened'
+  server.use(
+    rest.post(
+      'https://auth-provider.example.com/api/login',
+      async (req, res, ctx) => {
+        return res(ctx.status(500), ctx.json({message: testErrorMessage}))
+      },
+    ),
+  )
+
+  render(<Login />)
+  await userEvent.click(screen.getByRole('button', {name: /submit/i}))
+
+  await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+  expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
