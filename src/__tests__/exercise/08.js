380f0b46ea3c5422d67a56bca015fff8d567d4a4
RayanaSales <rayanaasales@gmail.com>
2024-06-29 00:06:27 -0300

    Exercise 08: increase test coverage

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -5,20 +5,24 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', async () => {
-  let result
+function setup({initialProps} = {}) {
+  const result = {}
   function TestComponent() {
-    result = useCounter()
+    result.current = useCounter(initialProps)
     return null
   }
-
   render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
-  // console.log(result);
+  return result
+}
+
+test('exposes the count and increment/decrement functions', async () => {
+  let result = setup()
+
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 
   /**
    * [KCD] The act function is telling react: "hey im gonna do some stuff that might cause some updates, and after my callback its all finished I want you to flush all side effects from react. My component its stable".
@@ -26,4 +30,24 @@ test('exposes the count and increment/decrement functions', async () => {
    * This was not needed before, because we were using user events, which wrapps everything in act calls. But it's now necessary because we are calling the increment and decrement functions directly - and because of that you have to manually wrap it with act function.
    *
    */
+})
+
+test('allows customization of the initial count', async () => {
+  let result = setup({initialProps: {initialCount: 3}})
+
+  expect(result.current.count).toBe(3)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(4)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(3)
+})
+
+test('allows customization of the step', async () => {
+  let result = setup({initialProps: {step: 2}})
+
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(2)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
